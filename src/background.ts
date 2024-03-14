d458938dcd3eaeb9bcf14d7c6eb5afc8e7b40ecd
Roger Westerbø <roger.westerbo@nhn.no>
2024-03-14 16:49:25 +0100

    listening on tab updated
    removing console.logs

--- src/background.ts
+++ src/background.ts
@@ -1,29 +1,21 @@
 function checkAndCloseTab(tab: chrome.tabs.Tab): void {
-  console.log("start closeTab");
-  console.log("tab", tab);
-  
   if (
     tab?.pendingUrl?.indexOf("http://localhost:8020") !== -1 ||
     tab?.pendingUrl?.indexOf("http://127.0.0.1:8020") !== -1
   ) {
-    console.log("about to close tab");
     setTimeout(() => {
       chrome.tabs
         .remove(tab.id ? tab.id : -1)
-        .catch((error) => {
-          console.error(error);
-        })
         .then(() => {
-          console.log("tab closed");
+          console.log(`tab closed with title ${tab?.title} url ${tab?.url}`);
         });
     }, 1000);
   } else {
-    console.log("tab not closed, url", tab.pendingUrl)
+    console.log(`tab not closed, url ${tab.url}, pendingUrl ${tab.pendingUrl}`);
   }
 }
 
 chrome.tabs.onCreated.addListener((tab) => {
-  console.log("onCreated tab");
   if (
     tab.url?.startsWith("chrome://") ||
     tab.url?.startsWith("chrome-extension://")
@@ -31,4 +23,17 @@ chrome.tabs.onCreated.addListener((tab) => {
     return undefined;
   }
   checkAndCloseTab(tab);
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  chrome.tabs.query(
+    {
+      url: ["http://localhost:8020/*", "http://127.0.0.1:8020/*"],
+    },
+    (tabs) => {
+      tabs.forEach((tab) => {
+        checkAndCloseTab(tab);
+      });
+    }
+  );
 });
